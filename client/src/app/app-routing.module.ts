6a5ba7838d1e5329995d0f88c03b8b2a94929c52
hirorittsu <zq6295migly@gmail.com>
2019-10-19 10:22:13 +0900

    [add]result画面追加

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'schedule-new', loadChildren: './schedule-new/schedule-new.module#ScheduleNewPageModule' },
   { path: 'schedule-list', loadChildren: './schedule-list/schedule-list.module#ScheduleListPageModule' },
   { path: 'schedule-id', loadChildren: './schedule-id/schedule-id.module#ScheduleIdPageModule' },
+  { path: 'result', loadChildren: './result/result.module#ResultPageModule' },
 ];
 
 @NgModule({
