4fdcad84081433e8682aa4bcaf4c998c8655b5b0
hirorittsu <zq6295migly@gmail.com>
2019-10-19 11:05:31 +0900

    [update]loginからhomeへのページ遷移実装

--- client/src/app/login/login.page.ts
+++ client/src/app/login/login.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+    constructor(
+        private router: Router
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    goHome() {
+        this.router.navigateByUrl('/home');
+    }
+
+    goNewAccount() {
+        alert("実装中です。");
+    }
 }
