61ac1fa853bd794aefd6a59ae0ebe1d1dfa707f5
hirorittsu <zq6295migly@gmail.com>
2019-10-19 10:20:42 +0900

    [add]schedule_new画面追加

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'schedule-new', loadChildren: './schedule-new/schedule-new.module#ScheduleNewPageModule' },
 ];
 
 @NgModule({
---
+++ client/src/app/schedule-new/schedule-new.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-new',
+  templateUrl: './schedule-new.page.html',
+  styleUrls: ['./schedule-new.page.scss'],
+})
+export class ScheduleNewPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
