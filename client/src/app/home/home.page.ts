aa3ef59dfeca1910451f37ab28dfcad0d25df19f
hirorittsu <zq6295migly@gmail.com>
2019-10-19 11:12:01 +0900

    [update]homeからスケジュール関連のページへの遷移実装

--- client/src/app/home/home.page.ts
+++ client/src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    constructor(
+        private router: Router,
+    ) {
+    }
+
+    goScheduleNew() {
+        this.router.navigateByUrl('/schedule-new');
+    }
+
+    goScheduleList() {
+        this.router.navigateByUrl('/schedule-list');
+    }
+
+    goFoundStore() {
+        alert("実装中♥");
+    }
 
 }
